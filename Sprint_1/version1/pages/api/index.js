a9a038c40acbeb34443d917138188841daf83eb1
Alvin Biju <bijualvin2@gmail.com>
2024-09-25 20:28:20 -0400

    Update index.js
    Signed-off-by: Alvin Biju <bijualvin2@gmail.com>

--- Sprint_1/version1/pages/api/index.js
+++ Sprint_1/version1/pages/api/index.js
@@ -1,4 +1,4 @@
-import Login from '../components/Login';
+/*import Login from '../components/Login';
 
 export default function Home() {
   return (
@@ -7,3 +7,4 @@ export default function Home() {
     </div>
   );
 }
+*/
