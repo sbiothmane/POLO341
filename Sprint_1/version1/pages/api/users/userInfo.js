802045397d6f930c447de7b72ae93225a3c44508
sbiothmane <sbi.othmane5@gmail.com>
2024-09-26 16:22:32 -0400

    remove console log

--- Sprint_1/version1/pages/api/users/userInfo.js
+++ Sprint_1/version1/pages/api/users/userInfo.js
@@ -1,14 +1,11 @@
 import { users, updateUsersArray } from './users';
 
 export default async function handler(req, res) {
-    updateUsersArray();
     if (req.method === 'POST') {
         const { username } = req.body;
-        console.log("req " + req.body);
-        console.log(users, username);
         const user = users.find((user) => user.username === username);
         user["password"] = undefined;
-        console.log(user);
+        
         if (user) {
             res.status(200).json(user);
         } else {
