3a607a1b42f1b74afbde60b46fcb0d6993f7dff6
William White <74478171+WilliamWhite1125@users.noreply.github.com>
2024-09-26 16:32:24 -0400

    Update tailwind.config.js
    Signed-off-by: William White <74478171+WilliamWhite1125@users.noreply.github.com>

--- Sprint_1/version1/tailwind.config.js
+++ Sprint_1/version1/tailwind.config.js
@@ -1,6 +1,10 @@
+//tailwind css config file
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+
+    //add more content here to expand
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
