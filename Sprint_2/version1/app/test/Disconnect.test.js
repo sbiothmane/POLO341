d4b5b43b71a33f4a66ea607215be47807852c3ac
Alvin Biju <bijualvin2@gmail.com>
2024-10-24 17:58:00 -0400

    Update Disconnect.test.js
    Signed-off-by: Alvin Biju <bijualvin2@gmail.com>

--- Sprint_2/version1/app/test/Disconnect.test.js
+++ Sprint_2/version1/app/test/Disconnect.test.js
@@ -1,1 +1,21 @@
+import React from 'react';  // Import React
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SignOutButton from './components/Disconnect';
+import { signOut } from 'next-auth/react';
+
+jest.mock('next-auth/react');
+
+describe('SignOutButton', () => {
+  test('renders the sign out button and triggers signOut when clicked', () => {
+    signOut.mockImplementation(() => {});
+
+    render(<SignOutButton />);
+    const button = screen.getByText('Sign Out');
+    expect(button).toBeInTheDocument();
+
+    button.click();
+    expect(signOut).toHaveBeenCalledTimes(1);
+  });
+});
 
