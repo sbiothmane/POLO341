1cccb771d58ce646f85f72f7cb934164eb023292
Alvin Biju <bijualvin2@gmail.com>
2024-10-24 21:40:18 -0400

    Update Loading.js
    Signed-off-by: Alvin Biju <bijualvin2@gmail.com>

--- Sprint_2/version1/app/components/Loading.js
+++ Sprint_2/version1/app/components/Loading.js
@@ -1,3 +1,5 @@
+'use client';
+import React from 'react';  // Import React
 export default function Loading() {
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4 sm:px-6 lg:px-8">
