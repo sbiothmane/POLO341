d001b0fee688aa5ba2ec95e7baf11b24e22929c6
Youssef Youssef <150083030+Youssef8811@users.noreply.github.com>
2024-11-21 13:59:34 -0500

    Update page.js
    Signed-off-by: Youssef Youssef <150083030+Youssef8811@users.noreply.github.com>

--- Sprint_4/App/app/OfficeHours/[instructor]/page.js
+++ Sprint_4/App/app/OfficeHours/[instructor]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import {
   motion,
