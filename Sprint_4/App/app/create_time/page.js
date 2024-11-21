09d9603870133bb2d636b68bfd9568b134aa5cbf
Alvin Biju <bijualvin2@gmail.com>
2024-11-21 17:40:38 -0500

    Update page.js
    Signed-off-by: Alvin Biju <bijualvin2@gmail.com>

--- Sprint_4/App/app/create_time/page.js
+++ Sprint_4/App/app/create_time/page.js
@@ -5,14 +5,14 @@ import { useSession } from 'next-auth/react'
 import { format, parse, addMinutes, isSameDay } from 'date-fns'
 import { toast, Toaster } from 'sonner'
 
-import AnimatedBackground from '@/components/home/AnimatedBackground'
-import Navbar from '@/components/home/Navbar'
+import AnimatedBackground from '@/app/components/home/AnimatedBackground'
+import Navbar from '@/app/components/home/Navbar'
 
-import CalendarComponent from '@/components/OfficeHours/CalendarComponent'
-import OfficeHoursList from '@/components/OfficeHours/OfficeHoursList'
-import CreateOfficeHoursDialog from '@/components/OfficeHours/CreateOfficeHoursDialog'
-import ReserveOfficeHourDialog from '@/components/OfficeHours/ReserveOfficeHourDialog'
-import Footer from '@/components/OfficeHours/Footer'
+import CalendarComponent from '@/app/components/OfficeHours/CalendarComponent'
+import OfficeHoursList from '@/app/components/OfficeHours/OfficeHoursList'
+import CreateOfficeHoursDialog from '@/app/components/OfficeHours/CreateOfficeHoursDialog'
+import ReserveOfficeHourDialog from '@/app/components/OfficeHours/ReserveOfficeHourDialog'
+import Footer from '@/app/components/OfficeHours/Footer'
 
 export default function OfficeHoursCalendar() {
   const { data: session, status } = useSession()
