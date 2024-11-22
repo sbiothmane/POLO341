6de86832ecd36edade5c86e878ed3db1eec0a9a7
Ukcl <johnkaspar11@gmail.com>
2024-11-22 13:03:54 -0500

    Update page.js
    Signed-off-by: Ukcl <johnkaspar11@gmail.com>

--- Sprint_4/App/app/create_poll/page.js
+++ Sprint_4/App/app/create_poll/page.js
@@ -214,7 +214,7 @@ const ChoicesField = ({ choices, onUpdate, onAdd, onRemove }) => (
     <AnimatePresence>
       {choices.map((choice, index) => (
         <motion.div
-          key={index}
+          key={`${choice}-${index}`}
           initial={{ opacity: 0, height: 0 }}
           animate={{ opacity: 1, height: 'auto' }}
           exit={{ opacity: 0, height: 0 }}
