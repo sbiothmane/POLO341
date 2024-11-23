469aa402557e86f110c8172482b2ef2e41641a39
sbiothmane <sbi.othmane5@gmail.com>
2024-11-23 14:01:35 -0500

    fix

--- Sprint_4/App/components/ui/calendar.jsx
+++ Sprint_4/App/components/ui/calendar.jsx
@@ -7,24 +7,20 @@ import { DayPicker } from "react-day-picker";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
-       const IconLeft = ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />;
-       const IconRight = ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />;
-
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  mode,
   ...props
 }) {
-  const IconLeft = (props) => <ChevronLeft className="h-4 w-4" {...props} />;
-  const IconRight = (props) => <ChevronRight className="h-4 w-4" {...props} />;
-
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        months:
+          "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
@@ -42,7 +38,7 @@ function Calendar({
         row: "flex w-full mt-2",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
-          props.mode === "range"
+          mode === "range"
             ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
             : "[&:has([aria-selected])]:rounded-md"
         ),
@@ -63,16 +59,16 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      {...props}
     />
   );
 }
-
 
 Calendar.propTypes = {
   className: PropTypes.string,
   classNames: PropTypes.object,
   showOutsideDays: PropTypes.bool,
-  mode: PropTypes.string, // Optional `mode` prop if used in `props`
+  mode: PropTypes.string,
 };
 
 Calendar.defaultProps = {
