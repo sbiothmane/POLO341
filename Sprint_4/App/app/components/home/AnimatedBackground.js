3a8f5a3e213a09cd1b54000bf270edbe508f1aec
Ukcl <johnkaspar11@gmail.com>
2024-11-23 08:59:14 -0500

    Update AnimatedBackground.js
    Signed-off-by: Ukcl <johnkaspar11@gmail.com>

--- Sprint_4/App/app/components/home/AnimatedBackground.js
+++ Sprint_4/App/app/components/home/AnimatedBackground.js
@@ -48,8 +48,8 @@ export default function AnimatedBackground() {
   return (
     <div className="fixed inset-0 z-0">
       <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={0.7} />
-          <directionalLight position={[10, 10, 10]} intensity={0.8} />
+        <ambientLight />
+          <pointLight position={[10, 10, 10]} intensity={0.8} />
 
         <AnimatedSphere />
         <Stars
