005be11b5de8731c12ad0bba68fd1344a954b67b
Ukcl <johnkaspar11@gmail.com>
2024-11-23 09:50:58 -0500

    Update avatar.jsx
    Signed-off-by: Ukcl <johnkaspar11@gmail.com>

--- Sprint_4/App/components/ui/avatar.jsx
+++ Sprint_4/App/components/ui/avatar.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import PropTypes from 'prop-types';
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
@@ -11,6 +12,11 @@ const Avatar = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
     {...props} />
 ))
+
+Avatar.propTypes = {
+  className: PropTypes.string,
+};
+
 Avatar.displayName = AvatarPrimitive.Root.displayName
 
 const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
@@ -19,6 +25,11 @@ const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("aspect-square h-full w-full", className)}
     {...props} />
 ))
+
+AvatarImage.propTypes = {
+  className: PropTypes.string,
+};
+
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
@@ -30,6 +41,11 @@ const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
     )}
     {...props} />
 ))
+
+AvatarFallback.propTypes = {
+  className: PropTypes.string,
+};
+
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
 export { Avatar, AvatarImage, AvatarFallback }
