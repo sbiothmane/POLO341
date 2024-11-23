00b45877fcc3462e97d0322262f2ba82d0214cb9
Ukcl <johnkaspar11@gmail.com>
2024-11-23 00:35:01 -0500

    Update AnimatedBackground.js
    Signed-off-by: Ukcl <johnkaspar11@gmail.com>

--- Sprint_4/App/app/components/home/AnimatedBackground.js
+++ Sprint_4/App/app/components/home/AnimatedBackground.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRef, useState } from 'react'
+import * as THREE from 'three';
 import { Sphere, MeshDistortMaterial, Float, Stars } from '@react-three/drei'
 import { useFrame,Canvas } from '@react-three/fiber'
 
